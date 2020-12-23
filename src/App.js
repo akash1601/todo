e3599d05b957288a3e94d7ec280ab449ae4e34c3
akash1601 <akashshah1601@gmail.com>
2020-12-22 22:41:39 -0800

    made structure for app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
+import React, { useState } from 'react';
 import './App.css';
-
+import Form from './components/Form';
+import TodoList from './components/ToDoList'
 function App() {
+  
   return (
     <div className="App">
       <header>
       <h1>Akash's ToDo List</h1>
+      
       </header>
+      <Form/>
+      <TodoList />
     </div>
   );
 }
+
 export default App;
